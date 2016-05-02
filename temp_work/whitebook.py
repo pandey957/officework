5be9b95075ed5b30fed196a6d268f1ab4f83ced0
Satyendra Raj Pandey <pandey957@gmail.com>
2016-05-02 11:41:32 +0530

    Added file to support unicode writing

--- temp_work/whitebook.py
+++ temp_work/whitebook.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
-import csv
+from unicodewriter import *
 from urllib import urlopen
-
+outfile = open('output.csv','wb')
+out_wrtr = UnicodeWriter(outfile)
+out_wrtr.writerow(['Category','CompanyName','Address','Region','Telephone','Fax','Email','Web_url'])
 def repeat_task(base_url , extend_url, return_set):
     new_url = base_url + extend_url
     new_data = BeautifulSoup(urlopen(new_url),'html.parser')
@@ -25,12 +27,12 @@ for link in url_links.findAll('a','catList'):
     new_url = base_url + extend_url
     output = set()
     links = repeat_task(base_url , extend_url, output)
-    print 'got links successfully'
     for line in links:
         data = BeautifulSoup(urlopen(line),'html.parser')
         contact_details = data.findAll('td','listDetails1')
         details = []
         for detail in contact_details:
             details.append(detail.text.strip())
-        print ', '.join(details)
-    break
+        address = ', '.join(details[2:-5])
+        out_wrtr.writerow([details[0].replace(',','/'),details[1],address.replace(',',' '),details[-5],details[-4],details[-3],details[-2], details[-1]])
+outfile.close()
